9c0a53c058acb4c357dccc29a3ecaf44af2edcb3
Tyler <tylerpham.9@gmail.com>
2018-11-10 17:03:51 -0800

    Added functions for calculating statistics
    calculates the mean, max, min, percentiles and standard deviation

--- load_data.R
+++ load_data.R
@@ -1,13 +1,13 @@
 get_period <- function(datetime){
   # From the Datetime datatype, create a column to represent what time period it is
-  # 9am, 6pm, 12am, not inclusive
-  period_end <- c(9, 18, 24)
+  # 5am-9am, 9am-5pm, 5pm-9pm, 9pm-5am not inclusive
+  period_end <- c(5, 9, 17, 21)
   
-  datetime$period[datetime$h < period_end[1]] <- "Morning"
-  datetime$period[datetime$h >= period_end[1] & datetime$h < period_end[2]] <- "Midday"
-  datetime$period[datetime$h >= period_end[2] & datetime$h < period_end[3]] <- "Evening"
+  datetime$period[datetime$h >= period_end[1] & datetime$h < period_end[2]] <- "Morning"
+  datetime$period[datetime$h >= period_end[2] & datetime$h < period_end[3]] <- "Midday"
+  datetime$period[datetime$h >= period_end[3] & datetime$h < period_end[4]] <- "Evening"
+  datetime$period[datetime$h >= period_end[4] | datetime$h < period_end[1]] <- "Late Night"
   
-  rm(period_end)
   return(datetime$period)
 }
 
@@ -22,13 +22,14 @@ format_data <- function(df){
   df$Weekday <- ifelse(df$Datetime$wday >= 1 & df$Datetime$wday <=5, TRUE, FALSE)
   df$Period <- get_period(df$Datetime)
   
-  train_data <- na.omit(train_data)
+  df <- na.omit(df)
   return(df)
 }
 
 train_data = read.table("data/Train Data.txt", 
                         header=TRUE, sep=',')
 train_data <- format_data(train_data)
+
 
 test1_data = read.table("data/test1.txt", 
                         header=TRUE, sep=',')
