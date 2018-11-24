724cbadf88c37e54b985498d1de2ea059e3477eb
lkoo <lkoo@sfu.ca>
2018-11-23 20:11:12 -0800

    finished the algorithm to find anomalies in with hmmm in both training and test data sets, as well as how to plot the resulting data. Has not applied to project data yet.

--- hmm.R
+++ hmm.R
@@ -12,6 +12,23 @@ projectsundaymorningtest1 <- subset(test1_data, Weekday == 0 &
                                       strptime(Time, "%H:%M:%S") >= strptime("08:00", "%H:%M") & 
                                       strptime(Time, "%H:%M:%S") < strptime("11:00", "%H:%M"))
 
+projectsundaymorningtest2 <- subset(test2_data, Weekday == 0 &
+                                      strptime(Time, "%H:%M:%S") >= strptime("08:00", "%H:%M") & 
+                                      strptime(Time, "%H:%M:%S") < strptime("11:00", "%H:%M"))
+
+projectsundaymorningtest3 <- subset(test3_data, Weekday == 0 & 
+                                      strptime(Time, "%H:%M:%S") >= strptime("08:00", "%H:%M") & 
+                                      strptime(Time, "%H:%M:%S") < strptime("11:00", "%H:%M"))
+
+projectsundaymorningtest4 <- subset(test4_data, Weekday == 0 & 
+                                      strptime(Time, "%H:%M:%S") >= strptime("08:00", "%H:%M") & 
+                                      strptime(Time, "%H:%M:%S") < strptime("11:00", "%H:%M"))
+
+projectsundaymorningtest5 <- subset(test5_data, Weekday == 0 & 
+                                      strptime(Time, "%H:%M:%S") >= strptime("08:00", "%H:%M") & 
+                                      strptime(Time, "%H:%M:%S") < strptime("11:00", "%H:%M"))
+
+
 death <- table(factor(format(projectsundaymorning$Date,"%D")))
 
 table(factor(format(projectsundaymorningtest1$Date,"%D")))
@@ -35,66 +52,103 @@ ans <- forwardbackward(modTest1f, return.all=TRUE, useC=TRUE)
 
 trainMaxArr=c(0,0,0,0,0)
 
-nrow(projectsundaymorning)
+trainDate <- vector()
+trainLog <- vector()
+trainLogSat <- vector()
+trainLogSun <- vector()
+
+isSun = T
+
 for(row in seq(from=1, to=55620, by=180)){
   
   dDate = projectsundaymorning[row,]$Date
   dDate = str_remove(dDate, " PST")
   dDate
   
+  trainDate <- c(trainDate, dDate)
+  
   dLoopTrain <- depmix(list(Global_active_power~1),
-                          data=subset(projectsundaymorning, Date==dDate),
-                          nstates=15,
-                          family=list(gaussian()))
+                       data=subset(projectsundaymorning, Date==dDate),
+                       nstates=15,
+                       family=list(gaussian()))
   dLoopTrainf <- setpars(dLoopTrain, getpars(fmTrain, which="pars"), which="pars")
   dLoopTrainLog <- forwardbackward(dLoopTrainf, return.all=TRUE, useC=TRUE)
+  
+  trainLog <- c(trainLog, dLoopTrainLog$logLike)
+  
+  if (isSun == T) {
+    trainLogSun <- c(trainLogSun, dLoopTrainLog$logLike)
+    isSun = F
+  } else {
+    trainLogSat <- c(trainLogSat, dLoopTrainLog$logLike)
+    isSun = T
+  }
   
   checkedMax = F
   
   if (checkedMax == F && dLoopTrainLog$logLike < trainMaxArr[1]) {
     trainMaxArr[1] = dLoopTrainLog$logLike
     checkedMax = T
-  } else {
-    checkedMax = F
   }
   
   if (checkedMax == F && dLoopTrainLog$logLike < trainMaxArr[2]) {
     trainMaxArr[2] = dLoopTrainLog$logLike
     checkedMax = T
-  } else {
-    checkedMax = F
   }
   
   if (checkedMax == F && dLoopTrainLog$logLike < trainMaxArr[3]) {
     trainMaxArr[3] = dLoopTrainLog$logLike
     checkedMax = T
-  } else {
-    checkedMax = F
   }
   
   if (checkedMax == F && dLoopTrainLog$logLike < trainMaxArr[4]) {
     trainMaxArr[4] = dLoopTrainLog$logLike
     checkedMax = T
-  } else {
-    checkedMax = F
   }
   
   if (checkedMax == F && dLoopTrainLog$logLike < trainMaxArr[5]) {
     trainMaxArr[5] = dLoopTrainLog$logLike
     checkedMax = T
-  } else {
-    checkedMax = F
   }
 }
 
 trainMaxArr
 
+length(trainDate)
+length(trainLog)
+min(trainDate)
+max(trainDate)
+min(trainLog)
+max(trainLog)
+
+trainLog
+
+trainLogSat
+
+length(trainLogSat)
+min(trainLogSat)
+max(trainLogSat)
+mean(trainLogSat)
+
 testMaxArr=c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+
+test1Date <- vector()
+test1Log <- vector()
+test1LogSat <- vector()
+test1LogSun <- vector()
+
+test1DateSat <- vector()
+test1DateSun <- vector()
+
+isSun = F
 
 for(row in seq(from=1, to=18360, by=180)){
   dDate = projectsundaymorningtest1[row,]$Date
   dDate = str_remove(dDate, " PST")
   dDate
+  
+  test1Date <- c(test1Date, dDate)
+  
   dLoopTest1 <- depmix(list(Global_active_power~1),
                        data=subset(projectsundaymorningtest1, Date==dDate),
                        nstates=15,
@@ -103,6 +157,19 @@ for(row in seq(from=1, to=18360, by=180)){
   dLoopTest1Log <- forwardbackward(dLoopTest1f, return.all=TRUE, useC=TRUE)
   
   dLoopTest1Log
+  
+  test1Log <- c(test1Log, dLoopTest1Log$logLike)
+  
+  if (isSun == T) {
+    test1LogSun <- c(test1LogSun, dLoopTest1Log$logLike)
+    test1DateSun <- c(test1DateSun, dDate)
+    isSun = F
+  } else {
+    test1LogSat <- c(test1LogSat, dLoopTest1Log$logLike)
+    test1DateSat <- c(test1DateSat, dDate)
+    isSun = T
+  }
+  
   checkedMax = F
   
   if (checkedMax == F && dLoopTest1Log$logLike < testMaxArr[1]) {
@@ -183,6 +250,348 @@ for(row in seq(from=1, to=18360, by=180)){
 
 testMaxArr
 
+test1Log
+
+length(test1Date)
+length(test1Log)
+min(test1Log)
+max(test1Log)
+
+test1Log
+
+test1DateSat
+test1LogSat
+
+length(test1LogSat)
+min(test1LogSat)
+max(test1LogSat)
+mean(test1LogSat)
+
+length(test1DateSat)
+min(test1DateSat)
+max(test1DateSat)
+
+quantile(test1LogSat, c(.10))
+test1Anomaly <- vector()
+test1AnomDate <- vector()
+
+for (i in 1:length(test1LogSat)) {
+  if (test1LogSat[i] > mean(test1LogSat)) {
+    next
+  }
+  if (test1LogSat[i] <= quantile(test1LogSat, c(.15))) {
+    test1Anomaly <- c(test1Anomaly, test1LogSat[i])
+    test1AnomDate <- c(test1AnomDate, test1DateSat[i])
+  }
+}
+
+test1Anomaly
+test1AnomDate
+
+plot(as.Date(test1DateSat), test1LogSat)
+points( as.Date(test1AnomDate), test1Anomaly, col="red")
+
+test2MaxArr=c(0,0,0,0,0)
+
+for(row in seq(from=1, to=18360, by=180)){
+  
+  dDate = projectsundaymorningtest2[row,]$Date
+  dDate = str_remove(dDate, " PST")
+  dDate
+  
+  dLoopTest2 <- depmix(list(Global_active_power~1),
+                       data=subset(projectsundaymorningtest2, Date==dDate),
+                       nstates=15,
+                       family=list(gaussian()))
+  dLoopTest2f <- setpars(dLoopTest2, getpars(fmTrain, which="pars"), which="pars")
+  dLoopTest2Log <- forwardbackward(dLoopTest2f, return.all=TRUE, useC=TRUE)
+  
+  checkedMax = F
+  
+  if (checkedMax == F &&  dLoopTest2Log$logLike < test2MaxArr[1]) {
+    test2MaxArr[1] =  dLoopTest2Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest2Log$logLike < test2MaxArr[2]) {
+    test2MaxArr[2] =  dLoopTest2Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest2Log$logLike < test2MaxArr[3]) {
+    test2MaxArr[3] =  dLoopTest2Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest2Log$logLike < test2MaxArr[4]) {
+    test2MaxArr[4] =  dLoopTest2Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest2Log$logLike < test2MaxArr[5]) {
+    test2MaxArr[5] =  dLoopTest2Log$logLike
+    checkedMax = T
+  }
+  
+  test2MaxArr
+}
+
+test2MaxArr
+
+test3MaxArr=c(0,0,0,0,0)
+
+for(row in seq(from=1, to=18360, by=180)){
+  
+  dDate = projectsundaymorningtest3[row,]$Date
+  dDate = str_remove(dDate, " PST")
+  dDate
+  
+  dLoopTest3 <- depmix(list(Global_active_power~1),
+                       data=subset(projectsundaymorningtest3, Date==dDate),
+                       nstates=15,
+                       family=list(gaussian()))
+  dLoopTest3f <- setpars(dLoopTest3, getpars(fmTrain, which="pars"), which="pars")
+  dLoopTest3Log <- forwardbackward(dLoopTest3f, return.all=TRUE, useC=TRUE)
+  
+  checkedMax = F
+  
+  if (checkedMax == F &&  dLoopTest3Log$logLike < test3MaxArr[1]) {
+    test3MaxArr[1] =  dLoopTest3Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest3Log$logLike < test3MaxArr[2]) {
+    test3MaxArr[2] =  dLoopTest3Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest3Log$logLike < test3MaxArr[3]) {
+    test3MaxArr[3] =  dLoopTest3Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest3Log$logLike < test3MaxArr[4]) {
+    test3MaxArr[4] =  dLoopTest3Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest3Log$logLike < test3MaxArr[5]) {
+    test3MaxArr[5] =  dLoopTest3Log$logLike
+    checkedMax = T
+  }
+  
+  test3MaxArr
+}
+
+test3MaxArr
+
+test4MaxArr=c(0,0,0,0,0)
+test4MinArr = c(0,0,0,0,0)
+
+for(row in seq(from=1, to=18360, by=180)){
+  
+  dDate = projectsundaymorningtest4[row,]$Date
+  dDate = str_remove(dDate, " PST")
+  dDate
+  
+  dLoopTest4 <- depmix(list(Global_active_power~1),
+                       data=subset(projectsundaymorningtest4, Date==dDate),
+                       nstates=15,
+                       family=list(gaussian()))
+  dLoopTest4f <- setpars(dLoopTest4, getpars(fmTrain, which="pars"), which="pars")
+  dLoopTest4Log <- forwardbackward(dLoopTest4f, return.all=TRUE, useC=TRUE)
+  
+  checkedMax = F
+  
+  if (checkedMax == F &&  dLoopTest4Log$logLike < test4MaxArr[1]) {
+    test4MaxArr[1] =  dLoopTest4Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest4Log$logLike < test4MaxArr[2]) {
+    test4MaxArr[2] =  dLoopTest4Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest4Log$logLike < test4MaxArr[3]) {
+    test4MaxArr[3] =  dLoopTest4Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest4Log$logLike < test4MaxArr[4]) {
+    test4MaxArr[4] =  dLoopTest4Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest4Log$logLike < test4MaxArr[5]) {
+    test4MaxArr[5] =  dLoopTest4Log$logLike
+    checkedMax = T
+  }
+  
+  
+  checkedMin = F
+  
+  if (checkedMin == F && test4MinArr[1] == 0) {
+    test4MinArr[1] = dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && dLoopTest4Log$logLike > test4MinArr[1]) {
+    test4MinArr[1] =  dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test4MinArr[2] == 0) {
+    test4MinArr[2] = dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  test4MinArr
+  
+  if (checkedMin == F &&  dLoopTest4Log$logLike > test4MinArr[2]) {
+    test4MinArr[2] =  dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test4MinArr[3] == 0) {
+    test4MinArr[3] = dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest4Log$logLike > test4MinArr[3]) {
+    test4MinArr[3] =  dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test4MinArr[4] == 0) {
+    test4MinArr[4] = dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest4Log$logLike > test4MinArr[4]) {
+    test4MinArr[4] =  dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test4MinArr[5] == 0) {
+    test4MinArr[5] = dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest4Log$logLike > test4MinArr[5]) {
+    test4MinArr[5] =  dLoopTest4Log$logLike
+    checkedMin = T
+  }
+  
+  test4MaxArr
+  
+  
+}
+
+test4MaxArr
+test4MinArr
+
+
+test5MaxArr=c(0,0,0,0,0)
+test5MinArr = c(0,0,0,0,0)
+
+for(row in seq(from=1, to=18360, by=180)){
+  
+  dDate = projectsundaymorningtest5[row,]$Date
+  dDate = str_remove(dDate, " PST")
+  dDate
+  
+  dLoopTest5 <- depmix(list(Global_active_power~1),
+                       data=subset(projectsundaymorningtest5, Date==dDate),
+                       nstates=15,
+                       family=list(gaussian()))
+  dLoopTest5f <- setpars(dLoopTest5, getpars(fmTrain, which="pars"), which="pars")
+  dLoopTest5Log <- forwardbackward(dLoopTest5f, return.all=TRUE, useC=TRUE)
+  
+  checkedMax = F
+  
+  if (checkedMax == F &&  dLoopTest5Log$logLike < test5MaxArr[1]) {
+    test5MaxArr[1] =  dLoopTest5Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest5Log$logLike < test5MaxArr[2]) {
+    test5MaxArr[2] =  dLoopTest5Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest5Log$logLike < test5MaxArr[3]) {
+    test5MaxArr[3] =  dLoopTest5Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest5Log$logLike < test5MaxArr[4]) {
+    test5MaxArr[4] =  dLoopTest5Log$logLike
+    checkedMax = T
+  }
+  
+  if (checkedMax == F &&  dLoopTest5Log$logLike < test5MaxArr[5]) {
+    test5MaxArr[5] =  dLoopTest5Log$logLike
+    checkedMax = T
+  }
+  
+  
+  checkedMin = F
+  
+  if (checkedMin == F && test5MinArr[1] == 0) {
+    test5MinArr[1] = dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && dLoopTest5Log$logLike > test5MinArr[1]) {
+    test5MinArr[1] =  dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test5MinArr[2] == 0) {
+    test5MinArr[2] = dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest5Log$logLike > test5MinArr[2]) {
+    test5MinArr[2] =  dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test5MinArr[3] == 0) {
+    test5MinArr[3] = dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest5Log$logLike > test5MinArr[3]) {
+    test5MinArr[3] =  dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test5MinArr[4] == 0) {
+    test5MinArr[4] = dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest5Log$logLike > test5MinArr[4]) {
+    test5MinArr[4] =  dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F && test5MinArr[5] == 0) {
+    test5MinArr[5] = dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+  if (checkedMin == F &&  dLoopTest5Log$logLike > test5MinArr[5]) {
+    test5MinArr[5] =  dLoopTest5Log$logLike
+    checkedMin = T
+  }
+  
+}
+
+test5MaxArr
+test5MinArr
 
 d12 <- depmix(list(Global_active_power~1),
               data=subset(projectsundaymorningtest1, Date=="2009-12-12"),
